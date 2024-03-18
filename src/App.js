b1b900231856296696bd4856106d0f6e4980de7a
Mariza0 <maria.net@mail.ru>
2024-03-18 16:28:47 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
 import { Listing } from './components/Listing';
 import jsonData from "./json/Etsy.json";
-// import { TypeData } from './components/TypeData';
-// import data from './json/Etsy.json';
-// const jsonArray = JSON.parse(data);
 
-// // interface Data {
-// //   key: string;
-// //   value: number;
-// // }
-
-// const jsonDataTyped: TypeData[] = jsonData as TypeData[];
-
-// // Теперь вы можете использовать jsonDataTyped, зная его структуру
-// console.log(jsonDataTyped);
 const jsonString = JSON.stringify(jsonData);
 const jsonArray = JSON.parse(jsonString);
 
